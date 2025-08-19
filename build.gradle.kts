0369f070c2bdad20170c6122399f5c328b625dc8
LectWolf <67421358+LectWolf@users.noreply.github.com>
2025-08-19 20:52:01 +0800

    Update build.gradle.kts
    Spigot Api 1.21.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
     // compileOnly("org.spigotmc:spigot:1.20") // NMS
 
     compileOnly("me.clip:placeholderapi:2.11.6")
